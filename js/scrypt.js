0b06a01e45f4f04af1a36c9b420c14b87238f8c2
Mikhail <fregat59-mk@yandex.ru>
2022-10-15 09:52:30 +0500

    Second Task Completed

--- js/scrypt.js
+++ js/scrypt.js
@@ -8,13 +8,37 @@ let personalMovieDB = {
 	privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?");
-const b = prompt("На сколько оцените его?");
-const c = prompt("Один из последних просмотренных фильмов?");
-const d = prompt("На сколько оцените его?");
+if (personalMovieDB.count < 10) {
+	console.log("Просмотрено слишком мало фильмов!");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+	console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+	console.log("Вы киноман");
+} else {
+	console.log("Произошла ошибка");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+// 	const a = prompt("Один из последних просмотренных фильмов?");
+// 	const b = prompt("На сколько оцените его?");
+// 	if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+// 		personalMovieDB.movies[a] = b;
+// 	} else {
+// 		i--;
+// 	}
+// }
+
+let i = 0;
+while (i < 2) {
+	const a = prompt("Один из последних просмотренных фильмов?");
+	const b = prompt("На сколько оцените его?");
+	if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+		personalMovieDB.movies[a] = b;
+	} else {
+		i--;
+	}
+	i++;
+}
 
 
 console.log(personalMovieDB);
